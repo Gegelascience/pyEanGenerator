c7a1d9b94826e04fd3f9c6e103799c20cafe9d97
VERCHERE Rémi <remi.verchere2@gmail.com>
2022-04-18 14:36:42 +0200

    V1 ok

--- EanGenerator/Ean13Generator.py
+++ EanGenerator/Ean13Generator.py
@@ -1,4 +1,101 @@
+from Utils import setA, setB, setC, SpecialChar
+from EanCheck.EanCheckHelper import EanCheckHelper, EanType
+
+import tkinter as tk
+from tkinter import Canvas
+
+class InvalidEan13(Exception):
+    
+    def __init__(self):
+        self.message = "Invalid EAN13"
+
 class Ean13Generator:
 
-    def __init__(self):
-        pass
+    eanValue:str = None
+    barcodeValue:str = None
+
+    def __init__(self,value:str):
+        if EanCheckHelper.isCorrectEan(value, EanType.EAN13):
+            self.eanValue = value
+            self.__calculateBareCodeValue()
+
+        else:
+            raise InvalidEan13
+
+
+    def __calculateBareCodeValue(self):
+        self.barcodeValue = SpecialChar.START.value
+
+        firstPartRaw = self.eanValue[1:7]
+        secondPartRaw = self.eanValue[7:]
+
+        prefix = self.eanValue[0]
+
+        for index, element in enumerate(firstPartRaw):
+            setToApply = self.__calculateSetFromPrefix(prefix,index)
+            if setToApply == "A":
+                self.barcodeValue = self.barcodeValue + setA[element]
+            else:
+                self.barcodeValue = self.barcodeValue + setB[element]
+
+        self.barcodeValue = self.barcodeValue + SpecialChar.CENTER.value
+
+        for element in secondPartRaw:
+            self.barcodeValue = self.barcodeValue + setC[element]
+
+        self.barcodeValue = self.barcodeValue + SpecialChar.END.value
+
+    def __calculateSetFromPrefix(self, prefix:str, index:int) -> str:
+        if index == 0:
+            return "A"
+
+        if prefix == 0:
+            return "A"
+
+        elif prefix == "1":
+            return "A" if index in [1,3] else "B"
+
+        elif prefix == "2":
+            return "A" if index in [1,4] else "B"
+
+        elif prefix == "3":
+            return "A" if index in [1,5] else "B"
+
+        elif prefix == "4":
+            return "A" if index in [2,3] else "B"
+
+        elif prefix == "5":
+            return "A" if index in [3,4] else "B"
+
+        elif prefix == "6":
+            return "A" if index in [4,5] else "B"
+
+        elif prefix == "7":
+            return "A" if index in [2,4] else "B"
+
+        elif prefix == "8":
+            return "A" if index in [2,5] else "B"
+
+        elif prefix == "9":
+            return "A" if index in [3,5] else "B"
+
+    def showBarcode(self):
+        app = tk.Tk()
+        app.title(self.eanValue)
+        app.geometry("700x200")
+        canvas = Canvas(app)
+        canvas.pack()
+
+        width = 4
+        index = 10
+        for el in self.barcodeValue:
+            if el == "1":
+                canvas.create_line(index, 10, index, 50, width=width)
+            index = index + width
+
+        app.mainloop()
+
+
+if __name__ == "__main__":
+    test = Ean13Generator("3666154117284")
+    test.showBarcode()
--- EanGenerator/Ean8Generator.py
+++ EanGenerator/Ean8Generator.py
@@ -1,23 +1,60 @@
 from Utils import setA, setC, SpecialChar
-
 from EanCheck.EanCheckHelper import EanCheckHelper, EanType
 
+import tkinter as tk
+from tkinter import Canvas
 
 class InvalidEan8(Exception):
     
     def __init__(self):
-        self.message = "Invalid EAN 8"
+        self.message = "Invalid EAN8"
 
 class Ean8Generator:
 
     eanValue:str = None
+    barcodeValue:str = None
 
     def __init__(self,value:str):
         if EanCheckHelper.isCorrectEan(value, EanType.EAN8):
             self.eanValue = value
+            self.__calculateBareCodeValue()
 
         else:
             raise InvalidEan8
 
+    def __calculateBareCodeValue(self):
+        self.barcodeValue = SpecialChar.START.value
 
-Ean8Generator(None)
+        firstPartRaw = self.eanValue[:4]
+        secondPartRaw = self.eanValue[4:]
+
+        for element in firstPartRaw:
+            self.barcodeValue = self.barcodeValue + setA[element]
+
+        self.barcodeValue = self.barcodeValue + SpecialChar.CENTER.value
+
+        for element in secondPartRaw:
+            self.barcodeValue = self.barcodeValue + setC[element]
+
+        self.barcodeValue = self.barcodeValue + SpecialChar.END.value
+
+    def showBarcode(self):
+        app = tk.Tk()
+        app.title(self.eanValue)
+        app.geometry("700x200")
+        canvas = Canvas(app)
+        canvas.pack()
+
+        width = 4
+        index = 10
+        for el in self.barcodeValue:
+            if el == "1":
+                canvas.create_line(index, 10, index, 50, width=width)
+            index = index + width
+
+        app.mainloop()
+
+
+if __name__ == "__main__":
+    test = Ean8Generator("36661541")
+    test.showBarcode()
